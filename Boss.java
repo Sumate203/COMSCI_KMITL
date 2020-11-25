aa8a78a34e6e3cf483135e006171da4e2ac687b7
Sumate203 <73338558+Sumate203@users.noreply.github.com>
2020-11-25 21:25:40 +0700

    Update Boss.java

--- Boss.java
+++ Boss.java
@@ -7,17 +7,17 @@ public class Boss {
     private SpriteSheet enemyss;
 
     public Boss() {
-
+       
         enemylocation = LoadImages.loadImages("/pic/too.png");
-        enemyss = new SpriteSheet(enemylocation);
+        enemyss = new SpriteSheet(enemylocation);  
         getBoss();
-
+        
     }
 
     public void getBoss() {
         enemy[0] = enemyss.crop(0, 0, 60, 60);
-
+        
     }
-
-
+   
+    
 }
